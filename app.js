ac5db8d4ee13b758d3a4a5289d83bdfb05fecac8
tyler <tylercobb92@gmail.com>
2021-09-23 17:36:10 -0400

    Questions 1-8 in part one complete

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 $('DOM').ready(function () {
     console.log("Let's get ready to party with jQuery!")
+});
+
+$('img').addClass('image-center');
+
+$('p').last().remove();
+
+$('h1').css('font-size', Math.floor(Math.random() * 101));
+
+$('ol').append('<li>Puppies are great!</li>');
+
+$('aside').replaceWith("<p>I apologize for the list, it won't happen again!</p>");
+
+$('body').on('change', 'input', function () {
+    let $R = $('input').eq(0).val();
+    let $G = $('input').eq(2).val();
+    let $B = $('input').eq(1).val();
+    $('body').css('background-color', 'rgb(' + $R + ',' + $G + ',' + $B + ')');
 })
 
-$('img').addClass('image-center')
+$('img').on('click', function () {
+    $(this).remove()
+})
+
